421b6d6545eb03ee43ee44127af0ca57b81c1d95
Mia Elias <mia.te.408@gmail.com>
2023-01-26 09:25:01 +0200

    My year of study

--- Me.java
+++ Me.java
@@ -1,6 +1,7 @@
 public class Me {
 	public static void main(String[] args){
 		System.out.println("Mia Tony Elias");
-		System.out.println("Electrical Engineer");
+		System.out.print("Electrical Engineer");
+		System.out.println(" since 2022");
 	}
 }
