3148ef8a684791de39859cc27e924d861fe86f29
Mia Elias <mia.te.408@gmail.com>
2023-01-26 09:06:37 +0200

    corrected problem

---
+++ Problem.java
@@ -0,0 +1,6 @@
+public class Problem {
+	public static void main (String[] args) {
+		System.out.print("Hello, I have a lot of problems here");
+		System.out.println("Can You HELP me in that plz?");
+	}
+}
